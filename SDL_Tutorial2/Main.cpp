1cb155a7102c603463cff5340c4f4de3fcaa80d9
Enes Baltacı <enesniyazi.baltaci@ogr.deu.edu.tr>
2021-10-09 16:15:59 +0300

    MOVEMENT BETA

--- SDL_Tutorial2/Main.cpp
+++ SDL_Tutorial2/Main.cpp
@@ -55,13 +55,30 @@ int main(int argc, char* args[])
 
 	SDL_Texture* endGame = window.loadTexture("game_over.png");
 
+	SDL_Texture* knight_w1 = window.loadTexture("hulking_knight_walk1.png");
+
+	SDL_Texture* knight_w2 = window.loadTexture("hulking_knight_walk2.png");
+
+	SDL_Texture* knight_w3 = window.loadTexture("hulking_knight_walk3.png");
+
 	std::vector<Entity> entitiees = { Entity(Vector2f(0, 0), background, 320, 180),
 									  Entity(Vector2f(0, 630), platform, 320, 25),
 									  Entity(Vector2f(50, 0), knight, 64, 60),
 									  Entity(Vector2f(1280, 630), secondPlatform, 320, 25),
-									  Entity(Vector2f(250, 70), endGame, 200, 100) };
+									  Entity(Vector2f(250, 70), endGame, 200, 100),
+									  Entity(Vector2f(50, 0), knight, 64, 60),
+									  Entity(Vector2f(50, 0), knight_w1, 64, 60),
+									  Entity(Vector2f(50, 0), knight_w2, 64, 60),
+									  Entity(Vector2f(50, 0), knight_w3, 64, 60) };
 
-	bool gameRunning = 1, mouse = 0, down = 0, newPlat = 0, gameOver = 0;
+	bool gameRunning = 1, mouse = 0, down = 0, newPlat = 0, gameOver = 0, change[4];
+
+	int changeIndex = 0;
+
+	for (size_t i = 0; i < 4; i++)
+	{
+		change[i] = 0;
+	}
 
 	SDL_Event event;
 
@@ -81,6 +98,10 @@ int main(int argc, char* args[])
 		window.display();
 
 		entitiees[PLAYER_ENTITY_ID].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y + 1);
+		entitiees[5].setPos(entitiees[5].getPos().x, entitiees[5].getPos().y + 1);
+		entitiees[6].setPos(entitiees[6].getPos().x, entitiees[6].getPos().y + 1);
+		entitiees[7].setPos(entitiees[7].getPos().x, entitiees[7].getPos().y + 1);
+		entitiees[8].setPos(entitiees[8].getPos().x, entitiees[8].getPos().y + 1);
 	}
 
 	while (gameRunning)
@@ -119,10 +140,18 @@ int main(int argc, char* args[])
 			if (entitiees[PLAYER_ENTITY_ID].getPos().y > 100 && !down)
 			{
 				entitiees[PLAYER_ENTITY_ID].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y - 1);
+				entitiees[5].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y - 1);
+				entitiees[6].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y - 1);
+				entitiees[7].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y - 1);
+				entitiees[8].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y - 1);
 			}
 			else
 			{
 				entitiees[PLAYER_ENTITY_ID].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y + 1);
+				entitiees[5].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y + 1);
+				entitiees[6].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y + 1);
+				entitiees[7].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y + 1);
+				entitiees[8].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y + 1);
 				down = 1;
 			}
 
@@ -161,12 +190,50 @@ int main(int argc, char* args[])
 		{
 			if (entitiees[PLAYER_ENTITY_ID].getMid().x < 620 && (entitiees[PLAYER_ENTITY_ID].getMid().x - mouseX < 150 && entitiees[PLAYER_ENTITY_ID].getMid().x - mouseX > 0 && mouseY - entitiees[PLAYER_ENTITY_ID].getPos().y < 200 && mouseY - entitiees[PLAYER_ENTITY_ID].getPos().y > 0))
 			{
-				entitiees[PLAYER_ENTITY_ID].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x + 1, entitiees[PLAYER_ENTITY_ID].getPos().y);
+				if (change[3]) change[3] = 0;
+
+				if (!change[0] && changeIndex >= 0 && changeIndex < 10)
+				{
+					entitiees[PLAYER_ENTITY_ID] = entitiees[6];
+					changeIndex++;
+					if (changeIndex == 10) change[0] = 1;
+				}
+				else if (!change[1] && changeIndex >= 10 && changeIndex < 20)
+				{
+					entitiees[PLAYER_ENTITY_ID] = entitiees[7];
+					changeIndex++;
+					if (changeIndex == 20) change[1] = 1;
+				}
+				else if (changeIndex >= 20 && changeIndex < 30)
+				{
+					entitiees[PLAYER_ENTITY_ID] = entitiees[8];
+					changeIndex++;
+					if (changeIndex == 30)
+					{
+						change[0] = 0;
+						change[1] = 0;
+						changeIndex = 0;
+					}
+				}
+				entitiees[PLAYER_ENTITY_ID].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x + 1, entitiees[PLAYER_ENTITY_ID].getPos().y); // move right with mouse
+				entitiees[5].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y);
+				entitiees[6].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y);
+				entitiees[7].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y);
+				entitiees[8].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y);
 			}
 			else if (entitiees[PLAYER_ENTITY_ID].getMid().x > 100)
 			{
+				if (!change[3])
+				{
+					entitiees[PLAYER_ENTITY_ID] = entitiees[5];
+					change[3] = 1;
+				}
 				SDL_Delay(1);
-				entitiees[PLAYER_ENTITY_ID].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x - 1, entitiees[PLAYER_ENTITY_ID].getPos().y);
+				entitiees[PLAYER_ENTITY_ID].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x - 1, entitiees[PLAYER_ENTITY_ID].getPos().y); // move left without mouse
+				entitiees[5].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y);
+				entitiees[6].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y);
+				entitiees[7].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y);
+				entitiees[8].setPos(entitiees[PLAYER_ENTITY_ID].getPos().x, entitiees[PLAYER_ENTITY_ID].getPos().y);
 			}
 		}
 		else
